b4b3be1229b85365b7db19446da49e19fbf9b326
moisesfilho05 <filhomoises1708@gmail.com>
2025-01-24 23:34:03 -0300

    refactor: percentage function

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ let quantidadeMilhas = parseInt(prompt('Digite a quantidade de Milhas que você 
 let porcentagemDesconto = 0;
 let iphone16ProMax = 17.358;
 
+function porcentagem(valorDesconto) {
+    let porcentagem = (valorDesconto / 100) * iphone16ProMax;
+    let valorFinal = iphone16ProMax - porcentagem;
+    alert(`Parabéns! Você ganhou ${valorDesconto}% de desconto na compra do seu Iphone 16 Pro Max.
+    Valor do Iphone: ${iphone16ProMax}. Desconto: ${porcentagem.toFixed(3)}. Preço final: ${valorFinal.toFixed(3)}.`);
+};
+
 if (quantidadeMilhas >= 30000) {
-    let porcentagem = (20 / 100) * iphone16ProMax;
-    let desconto = iphone16ProMax - porcentagem;
-    alert(`Parabéns, você ganhou um desconto de 20% na compra do seu Iphone 16 Pro Max.
-    Desconto de ${porcentagem.toFixed(3)} em cima do Valor do Iphone> ${iphone16ProMax}. Preço com desconto: ${desconto.toFixed(3)}.`)
+    porcentagem(20);
 } else {
     if(quantidadeMilhas < 5000) {
-        alert(`Quantidada de Milhas Insufiente para ganhar desconto. Valor do Iphone: ${iphone16ProMax}`);
+        alert(`Quantidade de Milhas Insufiente.`);
     } else {
-        let porcentagem2 = (10 / 100) * iphone16ProMax;
-        let desconto2 = iphone16ProMax - porcentagem2;
-        alert(`Parabéns, você ganhou um desconto de 10% na compra do seu Iphone 16 Pro Max.
-    Desconto de ${porcentagem2.toFixed(3)} em cima do Valor do Iphone> ${iphone16ProMax}. Preço com desconto: ${desconto2.toFixed(3)}.`)
+        porcentagem(10);
     };
 };
